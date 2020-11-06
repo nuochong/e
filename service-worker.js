d766f79f0005db8df91fd394a95a78d9668f7c0c
nuochong <nuo.chong@163.com>
2020-11-07 00:00:23 +0800

    robot

--- service-worker.js
+++ service-worker.js
@@ -1114,11 +1114,11 @@ self.__precacheManifest = [
     "revision": "0271ffe986e34b0d8dc3c9ce4b5af31e"
   },
   {
-    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/18.styles.21a26cd0.css",
+    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/18.styles.37ed721b.css",
     "revision": "ea66d298ade686cc900755bde519c637"
   },
   {
-    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/19.styles.cfec79cf.css",
+    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/19.styles.f21b8763.css",
     "revision": "86e463286683c49c2c08b770630cb28c"
   },
   {
@@ -1134,19 +1134,19 @@ self.__precacheManifest = [
     "revision": "866133fd695e5edd0678b6a4c0b3663e"
   },
   {
-    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/24.styles.add931f2.css",
+    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/24.styles.e4ee3fec.css",
     "revision": "66966f5a94d28a9f757aafc718fb67d6"
   },
   {
-    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/25.styles.f8b46131.css",
+    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/25.styles.689e779e.css",
     "revision": "bcf9eca32341b75323122b762fa9abb3"
   },
   {
-    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/26.styles.1251ed8f.css",
+    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/26.styles.652b51fb.css",
     "revision": "62d73c7f95b2cb4ab2de48175ee3393b"
   },
   {
-    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/27.styles.96fa71c1.css",
+    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/27.styles.59b41fa3.css",
     "revision": "ef5ad4490dffcdfee5cbf9869146a5ac"
   },
   {
@@ -1210,7 +1210,7 @@ self.__precacheManifest = [
     "revision": "e5993078e223b8b0317bf1e538260a5b"
   },
   {
-    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/styles.00f54354.css",
+    "url": "https://cdn.jsdelivr.net/gh/nuochong/e/assets/css/styles.c038ed46.css",
     "revision": "fa17ab7723246230d8c440ef4cd6a493"
   },
   {
